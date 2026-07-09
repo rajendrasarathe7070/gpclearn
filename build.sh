85e987b25457240bea83fa8a1d97c01a5a647f9b
rajendra123444 <rajendrasarathe144@gmail.com>
2026-07-09 19:04:29 +0530

    raj12

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 set -o errexit
-
 pip install -r requirements.txt
-
 python manage.py migrate
-
 python manage.py collectstatic --noinput
-
 # Create a superuser if it doesn't exist (optional)
 # python manage.py shell < setup_superuser.py
