b6df27bac637a9ead61232e082e574fa99c1be26
Rajendra Sarathe <143543512+rajendrasarathe7070@users.noreply.github.com>
2026-07-11 13:23:56 +0000

    python manage.py makemigrations

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -o errexit
 pip install -r requirements.txt
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 # Create a superuser if it doesn't exist (optional)
